e94ba58cb90ebf89703dc2da9cf4b6699f7feb0b
Your Name <charles.luas.young@gmail.com>
2021-12-24 08:23:34 -0700

    Vehicle Post, framework missing import

--- drf_jwt_capstone_backend/vehicle/urls.py
+++ drf_jwt_capstone_backend/vehicle/urls.py
@@ -4,4 +4,5 @@ from vehicle import views
 urlpatterns = [
     path('all/', views.get_all_vehicles),
     path('', views.user_vehicles),
+    path('create/', views.create_vehicle),
 ]
--- drf_jwt_capstone_backend/vehicle/views.py
+++ drf_jwt_capstone_backend/vehicle/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Vehicle
 from .serializers import VehicleSerializer
+from django.http import Http404
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -16,6 +17,18 @@ def get_all_vehicles(request):
     vehicle = Vehicle.objects.all()
     serializer = VehicleSerializer(vehicle, many=True)
     return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_vehicle(request):
+    serializer = VehicleSerializer( data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
